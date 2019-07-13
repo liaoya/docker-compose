4d70d01eb560e7f1609e5aa1e96de77d7bb085ba
Tom Shen <tom.shen@oracle.com>
2019-07-13 12:59:26 +0800

    Many version update

--- shadowsocks/libev/run.sh
+++ shadowsocks/libev/run.sh
@@ -7,5 +7,6 @@ if [[ -z ${SHADOWSOCK_PASSWORD} ]]; then
     echo "Please set \${SHADOWSOCK_PASSWORD} at first"
     exit 1
 fi
+PORT=${PORT:-$((RANDOM % 10000 + 10000))}
 
 docker-compose -f "${THIS_DIR}/docker-compose.yml" up -d
