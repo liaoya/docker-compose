0ba94c75d09ff95f4d9edd80fce75bcb66e42ccd
Tom Shen <tom.shen@oracle.com>
2019-08-17 22:53:20 +0800

    Fix v2ray

--- v2ray/vmess/env.sh
+++ v2ray/vmess/env.sh
@@ -9,7 +9,7 @@ if [[ -f "${THIS_DIR}/.options" ]]; then
     V2RAY_UUID=$(grep uuid "${THIS_DIR}/.options" | cut -d"=" -f2)
 else
     V2RAY_ALTERID=${V2RAY_ALTERID:-$((RANDOM%70+30))}
-    V2RAY_PORT=${V2RAY_PORT:-$((RANDOM%30000+20000))}
+    V2RAY_PORT=${V2RAY_PORT:-$((RANDOM%10000+30000))}
     V2RAY_UUID=${V2RAY_UUID:-$(cat /proc/sys/kernel/random/uuid)}
     { echo "alterid=${V2RAY_ALTERID}"; echo "port=${V2RAY_PORT}"; echo "uuid=${V2RAY_UUID}"; } >> "${THIS_DIR}/.options"
     if [[ -f "${THIS_DIR}/config.json" ]]; then rm -f "${THIS_DIR}/config.json"; fi
