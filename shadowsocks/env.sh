18ff0d2b4dfc771aea1a55561c8cdc75b4e9ad0a
Huifeng Shen <liaoya@gmail.com>
2021-07-30 12:02:34 +0800

    Update some version

--- shadowsocks/env.sh
+++ shadowsocks/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SHADOWSOCKS_LIBEV_VERSION=${SHADOWSOCKS_LIBEV_VERSION:-$(curl -sL "https://api.github.com/repos/shadowsocks/shadowsocks-libev/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')}
-SHADOWSOCKS_LIBEV_VERSION=${SHADOWSOCKS_LIBEV_VERSION:-v3.3.4}
+SHADOWSOCKS_LIBEV_VERSION=${SHADOWSOCKS_LIBEV_VERSION:-v3.3.5}
 KCPTUN_VERSION=${KCPTUN_VERSION:-$(curl -s "https://api.github.com/repos/xtaci/kcptun/tags" | jq -r '.[0].name')}
-KCPTUN_VERSION=${KCPTUN_VERSION:-v20200701}
+KCPTUN_VERSION=${KCPTUN_VERSION:-v20210624}
 
 export KCPTUN_VERSION SHADOWSOCKS_LIBEV_VERSION
