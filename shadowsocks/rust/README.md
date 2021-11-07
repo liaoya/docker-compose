7e47be432290163d50aea4cafbf30f81e9e46eeb
Huifeng Shen <liaoya@gmail.com>
2021-11-07 11:27:42 +0100

    Add rust shadowsocks

---
+++ shadowsocks/rust/README.md
@@ -0,0 +1,29 @@
+# README
+
+This is the same as kcptun except use shadowsocks-rust instead of shadowsocks-libev
+
+The client configuration must be the same as server side.
+
+The example of client json file
+
+```json
+{
+  "remoteaddr": "server:29900",
+  "localaddr": "0.0.0.0:4000",
+  "key": "password",
+  "crypt": "none",
+  "mode": "fast2",
+  "conn": 3,
+  "mtu": 1350,
+  "sndwnd": 256,
+  "rcvwnd": 512,
+  "parityshard": 3,
+  "sockbuf": 8388608,
+  "smuxver": 1,
+  "nocomp": true,
+  "acknodelay": false,
+  "quiet": true,
+  "tcp": false,
+  "log": "\/var\/log\/kcptun\/client.general.log"
+}
+```
---
+++ shadowsocks/rust/env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "${THIS_FILE}")
+
+if [[ -f "${THIS_DIR}/.options" ]]; then
+    KCPTUN_PORT=$(grep -e "^kcptun_port=" "${THIS_DIR}/.options" | cut -d"=" -f2)
+    SHADOWSOCK_PASSWORD=$(grep -e "^password=" "${THIS_DIR}/.options" | cut -d"=" -f2)
+    SHADOWSOCK_PORT=$(grep -e "^port=" "${THIS_DIR}/.options" | cut -d"=" -f2)
+else
+    KCPTUN_PORT=${KCPTUN_PORT:-$((RANDOM % 30000 + 10000))}
+    SHADOWSOCK_PASSWORD=${SHADOWSOCK_PASSWORD:-$(tr -cd '[:alnum:]' </dev/urandom | fold -w30 | head -n1)}
+    SHADOWSOCK_PORT=${SHADOWSOCK_PORT:-$((RANDOM % 30000 + 10000))}
+    {
+        echo "kcptun_port=${KCPTUN_PORT}"
+        echo "password=${SHADOWSOCK_PASSWORD}"
+        echo "port=${SHADOWSOCK_PORT}"
+    } >>"${THIS_DIR}/.options"
+fi
+export KCPTUN_PORT SHADOWSOCK_PASSWORD SHADOWSOCK_PORT
+
+envsubst "$(env | sort | sed -e 's/=.*//' -e 's/^/\$/g')" <"${THIS_DIR}/shadowsocks-rust-tpl.json" | tee "${THIS_DIR}/shadowsocks-rust.json"
